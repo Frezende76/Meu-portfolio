5273222885aa3f744cded16f26c698347f2f2b08
Fabricio Rezende <f_rezende@yahoo.com>
2023-08-02 16:49:25 -0300

    Update: Criando um componente no vue, do meu novo sistema

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,3 +1,22 @@
+<!-- Essa tag para usar o componente criado -->
 <template>
-  
+   <PrimeiroComponente />
 </template>
+
+<script>
+
+// Tenho que importar o arquivo PrimeiroComponente para meu arquivo app.vue entenda que ele existe
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+
+// Para o Vue saber que eu estou exportando o componente App
+
+  export default {
+    name: "App",
+
+    // Tenho que explicitar para o vue que teremos components e colocar o nome do componente criado para que ele possa ficar pronto para utilização
+    components: {
+      PrimeiroComponente
+    }
+  }
+</script>
+
