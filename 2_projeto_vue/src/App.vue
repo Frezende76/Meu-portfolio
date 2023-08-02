037e862dae050ac93267b6e44b68d879aef56eb3
Fabricio Rezende <f_rezende@yahoo.com>
2023-08-02 18:32:23 -0300

    Update: Entendendo os LifeCycles

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,21 +1,31 @@
 <!-- Essa tag para usar o componente criado -->
 <template>
-   <PrimeiroComponente />
+  <div>
+    <PrimeiroComponente />
+    <p>O dado do app é: {{ testando }}</p>
+    <LifeCycle />
+  </div>
 </template>
 
 <script>
 
-// Tenho que importar o arquivo PrimeiroComponente para meu arquivo app.vue entenda que ele existe
+// Tenho que importar os meus componentes para meu arquivo app.vue entenda que ele existe
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
 
 // Para o Vue saber que eu estou exportando o componente App
 
   export default {
     name: "App",
-
+    data(){
+      return{
+        testando:  'teste'
+      }
+    },
     // Tenho que explicitar para o vue que teremos components e colocar o nome do componente criado para que ele possa ficar pronto para utilização
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
     }
   }
 </script>
