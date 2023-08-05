e8b81fc4f636ee3dcebbf76ee12001ad71e3aa18
Fabricio Rezende <f_rezende@yahoo.com>
2023-08-04 22:19:11 -0300

    Update: Aprendendo a Reutilização de componentes de vue.js

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,18 +1,29 @@
 <template>
     <div>
+        <Reutilizacao />
         <h1>Ola Vue!</h1>
+        <Reutilizacao />
         <p> Meu nome é {{ nome }} e trabalho como {{ profissão }}</p>
         <multiplosEventos />
+        <Reutilizacao />
+        <Reutilizacao />
+        <Reutilizacao />
+        <Reutilizacao />
+        <Reutilizacao />
+        <Reutilizacao />
+
     </div>
 </template>
 
 <script>
     import MultiplosEventos from './MultiplosEventos.vue';
+    import Reutilizacao from './Reutilizacao.vue';
 
     export default {
         name: 'PrimeiroComponente',
         components: {
-            MultiplosEventos
+            MultiplosEventos,
+            Reutilizacao
         },
         data() {
             return {
