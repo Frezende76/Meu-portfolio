adeca04004de0a408360ab7e984a40cc504f5116
Fabricio Rezende <f_rezende@yahoo.com>
2023-08-10 00:13:00 -0300

    Update: Finalizado Cabeçalho e rodapé do projeto Make your Burger

--- 3_make_your_burguer/src/App.vue
+++ 3_make_your_burguer/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <navbar />
+    <navbar :logo="logo_src" :alt="app_name" />
     <router-view />
     <Footer />
   </div>
@@ -13,6 +13,12 @@ export default {
     components: {
       Navbar,
       Footer
+    },
+    data() {
+      return {
+        logo_src: "/img/logo.png",
+        app_name: "Make your Burguer"
+      }
     }
 
 }
@@ -20,5 +26,22 @@ export default {
 </script>
 
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
