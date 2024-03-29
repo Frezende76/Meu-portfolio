4086bb48cf7f61701b81143c6bcfa183cdbc4c2b
Fabricio <f_rezende@yahoo.com>
2024-03-29 12:00:22 -0300

    Aprendendo Angular. Pipes e two-way-binding

--- curso_angular/curso-angular-matheus-battisti/src/app/app.module.ts
+++ curso_angular/curso-angular-matheus-battisti/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,8 @@ import { EventosComponent } from './components/eventos/eventos.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +25,14 @@ import { ChangeNumberComponent } from './components/change-number/change-number.
     EventosComponent,
     ListRenderComponent,
     EmitterComponent,
-    ChangeNumberComponent
+    ChangeNumberComponent,
+    PipesComponent,
+    TwoWayBindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
